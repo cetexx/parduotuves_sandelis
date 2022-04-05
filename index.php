ea53688f2b52bca0a5fc5148225044599d69bb9f
sarunas <sarunas.g@sender.net>
2022-04-05 21:09:29 +0300

    created login form in homepage

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
 <?php
-echo 'labas';
-
+/**
+ * prisijungimas
+ * registracija su rolemis (sandelio darbuotojas, parduotuves darbuotojas)
+ *
+ * //sandelio darbuotojas
+ * prekiu kategorijos (duona, pieno produktai, kaceliarija ... )
+ * prekiu valdymas (prideti, atnaujinti, istrinti) - prekės yra priskirtos prie parduotuvės
+ * sandėlis - jame saugomos visos prekiu sarasas ir yra maksimalus vienetu skaicius
+ *
+ * parduotuviu uzsakymai is sandelio (sandelio istorija)
+ *
+ * //parduotuves darbuotojas
+ * parduotuviu valdymas (prideti, atnaujinti, istrinti) (bendra marzas, papildomos marzos pagal kategorijas arba akcijos)
+ * parduotuves prekiu informacija (galiojimo laikas, kiekis/vienetai, kaina
+ * prekes kurias reikejo ismesti (kuriu galiojimas baigiasi) parduotuve
+ *
+ * pirkejas ir jo krepselis(kur ir ka pirko ir kiek sumokejo)
+ *
+ * statistika (kiek parduotuves uzdirbo, kokios prekes populiariausios)
+ */
+?>
+<form action="/action_page.php">
+    <fieldset>
+        <legend>Prisijungimas:</legend>
+        Paštas :  <input type="email" id="email" name="email">
+        <br><br>
+        Slaptažodis:  <input type="password" id="password" name="password">
+        <br><br>
+        <input type="submit" value="Prisijungti">
+        <hr>
+        <a href="#">Registracija</a>
+    </fieldset>
+</form>
